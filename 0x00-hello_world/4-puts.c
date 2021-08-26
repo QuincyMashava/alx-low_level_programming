e1205582648628a8dc8e38bc02341870daea401e
QuincyMashava <mashavaquincy@gmail.com>
2021-08-26 07:26:59 -0700

    no spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 
 /**
  *main - Entry point
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
