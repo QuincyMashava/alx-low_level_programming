3a73b02ef4259e2c9a821801a182d5b378ad2fc2
QuincyMashava <mashavaquincy@gmail.com>
2021-08-31 05:26:41 -0700

    o prog

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,16 +7,11 @@
 
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
-
+char c[10] = "Holberton";
+int i;
+for(i =0; i < 10; i++){
+_putchar(c[i]);
+}
+_putchar("\n");
 return (0);
 }
