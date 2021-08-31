93c2071a5242fdd1caf614d294a4a616504e0c57
QuincyMashava <mashavaquincy@gmail.com>
2021-08-31 12:13:23 -0700

    whitespace

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
-#include "main.h" 
-
+#include "main.h"
 /**
 * print_times_table - print multiplication table up to n
 * @n: integer argument
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -27,12 +27,11 @@ a_head = a / 1000000000; /* break larger num into 2 parts */
 a_tail = a % 1000000000;
 b_head = b / 1000000000;
 b_tail = b % 1000000000;
-  
 for (; counter < 99; counter++)
 {
 overflow = (a_tail + b_tail) / 1000000000;
-sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
-sum_head = (a_head + b_head) + overflow;
+sum_tail = (a_tail + b_tail)-(1000000000 * overflow);
+sum_head = (a_head + b_head)+overflow;
 
 printf(", %lu%lu", sum_head, sum_tail);
 
