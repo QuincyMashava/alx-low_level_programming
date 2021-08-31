4e69d88994d719b41c8fad2549b7378a6a1d9e1e
QuincyMashava <mashavaquincy@gmail.com>
2021-08-31 13:34:12 -0700

    mnbvcxz

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,19 @@
 #include "main.h"
-
 /**
-* print_last_digit - print last digit of integer
-* @n: integer argument
-* Return: last digit of number
+* print_last_digit - return last digit
+* @n : number to check
+* Return:0 or 1
 */
 
 int print_last_digit(int n)
 {
+int nv;
+
 if (n < 0)
-n *= -1;
+nv = -1 * (n % 10);
+else
+nv = n % 10;
 
-_putchar('0' + (n % 10));
-
-return (n % 10);
+_putchar(nv + '0');
+return (nv);
 }
