c9f0f3055f15702ac7a291d379e164b898903aaa
QuincyMashava <mashavaquincy@gmail.com>
2021-08-31 14:13:00 -0700

    betty commits

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 
-char *c= "_putchar";
+char *c = "_putchar";
 int i;
-for(i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
 _putchar(c[i]);
 }
 _putchar('\n');
-return 0;
+return (0);
 }
