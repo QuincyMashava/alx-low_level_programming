68f77d66edf41a1697fe1872cc9f99c25a93bb35
QuincyMashava <mashavaquincy@gmail.com>
2021-09-02 11:32:12 -0700

    print int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * print_number - prints numbers
 * @n: number to be printed
@@ -10,25 +11,25 @@ void print_number ( int n )
 unsigned int tens, digit, positive = n;
 double t_beg = 1;
 
-if (n == 0)
+if ( n == 0 )
 _putchar('0');
 else
 {
-if (n < 0)
+if ( n < 0 )
 {
 positive = n * -1;
-_putchar('-');
+_putchar( '-' );
 }
 
-while (t_beg <= positive)
+while ( t_beg <= positive)
 t_beg *= 10;
 tens = t_beg / 10;
 
-while (tens >= 1)
+while ( tens >= 1 )
 {
 digit = positive / tens;
 _putchar(digit + '0');
-positive = (positive - (tens * digit));
+positive = ( positive - (tens * digit));
 tens /= 10;
 }
 }
