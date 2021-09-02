ffd196f5562e519e75393d06cb4283ea01656fd3
QuincyMashava <mashavaquincy@gmail.com>
2021-09-02 10:29:21 -0700

    print int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,20 +5,14 @@
 * Return:void
 */
 
-void print_number(int n)
+void print_number (int n)
 {
-// If number is smaller than 0, put a - sign
-// and change number to positive
 if (n < 0)
 {
 putchar('-');
 n = -n;
 }
-  
-// Remove the last digit and recur
-if (n/10)
-print(n/10);
-  
-// Print the last digit
-putchar(n%10 + '0');
+if (n / 10 != 0)
+print_number(n / 10);
+putchar((n % 10) + '0');
 }
