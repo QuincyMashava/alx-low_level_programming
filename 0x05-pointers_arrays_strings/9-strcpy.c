c808394a35fa6696fa743fa0f2f65ef3bcc615d2
QuincyMashava <mashavaquincy@gmail.com>
2021-09-06 11:31:57 -0700

    betty style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -37,17 +37,3 @@ dest[i] = src[i];
 
 return (dest);
 }
-
-/**
-* another method using pointer notation
-*char *_strcpy(char *dest, char *src)
-*{
-*while (*src)
-*{
-*dest = *src;
-*src++;
-*dest++;
-*}
-*return (dest);
-*}
-*/
