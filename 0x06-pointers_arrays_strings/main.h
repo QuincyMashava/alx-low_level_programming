91834d0129ba36f4d7482538c1795859e694a231
QuincyMashava <mashavaquincy@gmail.com>
2021-09-09 12:23:27 -0700

    magic correction

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -96,7 +96,7 @@ void print_number(int n);
 
 int main(void);
 
-char infinite_add(char *n1, char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 
 #endif
