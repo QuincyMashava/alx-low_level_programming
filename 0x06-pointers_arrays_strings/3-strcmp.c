7b2ac14d503caf33ea44a60e38a2e63921b2c85b
QuincyMashava <mashavaquincy@gmail.com>
2021-09-09 13:33:28 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,11 @@
 * Return: int that tells num spaces in between
 */
 
-int _strcmp (char * s1 , char * s2)
+int _strcmp (char *s1 , char *s2)
 {
 int i = 0;
 
-while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+ while ((*(s1 + i)) == (*(s2 + i)) && (*(s1 + i)))
 i++;
 
 if (*(s2 + i))
