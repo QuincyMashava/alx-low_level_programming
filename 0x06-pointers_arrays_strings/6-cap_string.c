65ba8ef705ed4ea9d888b301a1dd7fe29e599017
QuincyMashava <mashavaquincy@gmail.com>
2021-09-09 07:21:37 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ int i = 0;
 
 /* check first index for capital */
 if (s[i] >= 'a' && s[i] <= 'z')
-    s[i] = s[i] - 'a' + 'A';
+s[i] = s[i] - 'a' + 'A';
 i++;
 
 while (s[i] != '\0') /* iterate through string */
@@ -25,7 +25,7 @@ if ((s[i] >= 'a' && s[i] <= 'z')
 	      s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
 	      s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
 	      || s[i - 1] == '\n'))
-	s[i] = s[i] - 'a' + 'A';
+s[i] = s[i] - 'a' + 'A';
 i++;
 }
 
