eb678db3d437c1d5af16eb7c61d27cb5f48d88eb
QuincyMashava <mashavaquincy@gmail.com>
2021-09-23 18:26:24 -0700

    ad tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,22 +31,28 @@ int num, len, i, j;
 
 num = n;
 
-if (s1 == NULL) /* account for NULL strings */
+if (s1 == NULL)
+/* account for NULL strings */
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-if (num < 0) /* account for negative n bytes */
+if (num < 0)
+/* account for negative n bytes */
 return (NULL);
-if (num >= _strlen(s2)) /* account for n too big */
+if (num >= _strlen(s2))
+/* account for n too big */
 num = _strlen(s2);
 
-len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+len = _strlen(s1) + num + 1;
+/* +1 to account for null pointer */
 
-ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+ptr = malloc(sizeof(*ptr) *len);
+/* malloc and check for error */
 if (ptr == NULL)
 return (NULL);
 
-for (i = 0; s1[i] != '\0'; i++) /* concat */
+for (i = 0; s1[i] != '\0'; i++)
+/* concat */
 ptr[i] = s1[i];
 for (j = 0; j < num; j++)
 ptr[i + j] = s2[j];
