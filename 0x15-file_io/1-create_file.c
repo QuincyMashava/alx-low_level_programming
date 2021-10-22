24ad75e5a7cfcadd3156af607fa60c08911ddbf5
QuincyMashava <mashavaquincy@gmail.com>
2021-10-22 14:29:56 -0700

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,56 +1,55 @@
 #include "main.h"
 
 /**
- * _strlen - find length of string
- * @str: string
- * Return: length
- */
+* _strlen - find length of string
+* @str: string
+* Return: length
+*/
 int _strlen(char *str)
 {
-  int len;
+int len;
 
-  for (len = 0; str[len] != '\0'; len++)
-    ;
+for (len = 0; str[len] != '\0'; len++)
+;
 
-  return (len);
+return (len);
 }
 
 /**
- * create_file - creates file with permissions rw------- and writes content in
- * if file already exists, don't change permissions and just truncate it
- * @filename: name to give to new file
- * @text_content: writes this content into file
- * Return: 1 on success, -1 on error
+* create_file - creates file with permissions rw------- and writes content in
+* if file already exists, don't change permissions and just truncate it
+* @filename: name to give to new file
+* @text_content: writes this content into file
+* Return: 1 on success, -1 on error
 */
 
 int create_file(const char *filename, char *text_content)
 {
-  int filedes;
-  int new_wrote;
+int filedes;
+int new_wrote;
 
-  if (!filename)
-    return (-1);
+if (!filename)
+return (-1);
 
-  /* create with permissions if file doesn't exist, else truncate */
-  filedes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-  if (filedes == -1)
-    return (-1);
+/* create with permissions if file doesn't exist, else truncate */
+filedes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (filedes == -1)
+return (-1);
 
-  /* if nothing to write, just return newly created file */
-  if (!text_content)
-    {
-      close(filedes);
-      return (1);
-    }
+/* if nothing to write, just return newly created file */
+if (!text_content)
+{
+close(filedes);
+return (1);
+}
+/* write */
+new_wrote = write(filedes, text_content, _strlen(text_content));
+if (new_wrote == -1 || new_wrote != _strlen(text_content))
+{
+close(filedes);
+return (-1);
+}
 
-  /* write */
-  new_wrote = write(filedes, text_content, _strlen(text_content));
-  if (new_wrote == -1 || new_wrote != _strlen(text_content))
-    {
-      close(filedes);
-      return (-1);
-    }
-
-  close(filedes);
-  return (1);
+close(filedes);
+return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,54 +1,54 @@
 #include "main.h"
 
 /**
- * _strlen - find length of string
- * @str: string
- * Return: length
- */
+* _strlen - find length of string
+* @str: string
+* Return: length
+*/
 int _strlen(char *str)
 {
-  int len;
+int len;
 
-  for (len = 0; str[len] != '\0'; len++)
-    ;
+for (len = 0; str[len] != '\0'; len++)
+;
 
-  return (len);
+return (len);
 }
 
 /**
- * append_text_to_file - append text and only if file exists
- * @filename: file
- * @text_content: appends this content into file
- * Return: 1 on success, -1 on error
- */
+* append_text_to_file - append text and only if file exists
+* @filename: file
+* @text_content: appends this content into file
+* Return: 1 on success, -1 on error
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-  int filedes;
-  int new_wrote;
+int filedes;
+int new_wrote;
 
-  if (!filename)
-    return (-1);
+if (!filename)
+return (-1);
 
-  /* open file if it exists */
-  filedes = open(filename, O_WRONLY | O_APPEND);
-  if (filedes == -1)
-    return (-1);
+/* open file if it exists */
+filedes = open(filename, O_WRONLY | O_APPEND);
+if (filedes == -1)
+return (-1);
 
-  /* if nothing to write, still successful */
-  if (!text_content)
-    {
-      close(filedes);
-      return (1);
-    }
+/* if nothing to write, still successful */
+if (!text_content)
+{
+close(filedes);
+return (1);
+}
 
-  /* write */
-  new_wrote = write(filedes, text_content, _strlen(text_content));
-  if (new_wrote == -1 || new_wrote != _strlen(text_content))
-    {
-      close(filedes);
-      return (-1);
-    }
+/* write */
+new_wrote = write(filedes, text_content, _strlen(text_content));
+if (new_wrote == -1 || new_wrote != _strlen(text_content))
+{
+close(filedes);
+return (-1);
+}
 
-  close(filedes);
-  return (1);
+close(filedes);
+return (1);
 }
