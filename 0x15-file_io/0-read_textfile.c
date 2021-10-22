ec5a1c59897f3039b03951988edd1bd75c745b29
QuincyMashava <mashavaquincy@gmail.com>
2021-10-22 14:10:03 -0700

    betty correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,38 +13,38 @@ int filedes; /* file descriptor */
 ssize_t new_read, new_wrote;
 char *buffer;
 
-  if (filename == NULL)
-    return (0);
+if (filename == NULL)
+return (0);
 
-  /* open */
-  filedes = open(filename, O_RDONLY);
-  if (filedes == -1)
-    return (0);
+/* open */
+filedes = open(filename, O_RDONLY);
+if (filedes == -1)
+return (0);
 
-  /* malloc buffer */
-  buffer = malloc(sizeof(char) * letters);
-  if (buffer == NULL)
-    return (0);
+/* malloc buffer */
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
 
-  /* read */
-  new_read = read(filedes, buffer, letters);
-  if (new_read == -1)
-    {
-      free(buffer);
-      close(filedes);
-      return (0);
-    }
+/* read */
+new_read = read(filedes, buffer, letters);
+if (new_read == -1)
+{
+free(buffer);
+close(filedes);
+return (0);
+}
 
-  /* write */
-  new_wrote = write(STDOUT_FILENO, buffer, new_read);
-  if (new_wrote == -1)
-    {
-      free(buffer);
-      close(filedes);
-      return (0);
-    }
+/* write */
+new_wrote = write(STDOUT_FILENO, buffer, new_read);
+if (new_wrote == -1)
+{
+free(buffer);
+close(filedes);
+return (0);
+}
 
-  close(filedes);
-  return (new_read);
+close(filedes);
+return (new_read);
 
 }
